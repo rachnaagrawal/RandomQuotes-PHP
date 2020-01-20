cb83f6e71eedbf82014c059f3da7ef89e75ff71c
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-20 17:40:16 +1000

    Added tests

--- public/api/index.php
+++ public/api/index.php
@@ -1,20 +1,5 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 require '../../vendor/autoload.php';
-$app = new \Slim\App();
 
-$app->get('/quote', function (Request $request, Response $response) {
-    $authors = file("authors.txt", FILE_IGNORE_NEW_LINES);
-    $quotes = file("quotes.txt", FILE_IGNORE_NEW_LINES);
-    $randomIndex = rand(0,count($authors) - 1);
-
-    $response->withStatus(200)->write("{\"quote\": \"" . $quotes[$randomIndex] . "\", " .
-        "\"author\": \"" . $authors[$randomIndex] . "\", " .
-        "\"appVersion\": \"1.0.0\", " .
-        "\"environmentName\": \"" . getenv('ENVIRONMENT_NAME') . "\" " .
-        "}");
-    return $response;
-});
-
+$app = (new Routes())->get();
 $app->run();
