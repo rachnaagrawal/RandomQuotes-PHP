1500d7ca39a3c616712a718e6ec85454014e6f3f
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-20 16:53:32 +1000

    Fixed up API response

--- public/api/index.php
+++ public/api/index.php
@@ -5,7 +5,15 @@ require '../../vendor/autoload.php';
 $app = new \Slim\App();
 
 $app->get('/quote', function (Request $request, Response $response) {
-    $response->withStatus(200)->write("Welcome to the Adroit Library Demo.");
+    $authors = file("authors.txt", FILE_IGNORE_NEW_LINES);
+    $quotes = file("quotes.txt", FILE_IGNORE_NEW_LINES);
+    $randomIndex = rand(0,count($authors) - 1);
+
+    $response->withStatus(200)->write("{\"quote\": \"" . $quotes[$randomIndex] . "\", " .
+        "\"author\": \"" . $authors[$randomIndex] . "\", " .
+        "\"appVersion\": \"1.0.0\", " .
+        "\"environmentName\": \"" . getenv('ENVIRONMENT_NAME') . "\" " .
+        "}");
     return $response;
 });
 
