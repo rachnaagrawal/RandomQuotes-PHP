7a6f0544b4573c3ee19f203cb93cb18d93efb4d0
Matthew Casperson <matthewcasperson@gmail.com>
2021-02-10 12:14:34 +1000

    Updated dependencies

--- routes/Routes.php
+++ routes/Routes.php
@@ -1,19 +1,20 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
 class Routes
 {
     private $app;
     public function __construct()
     {
-        $app = new \Slim\App();
+        $app = AppFactory::create();
 
         $app->get('/api/quote', function (Request $request, Response $response) {
             $authors = file(__DIR__ . "/authors.txt", FILE_IGNORE_NEW_LINES);
             $quotes = file(__DIR__ . "/quotes.txt", FILE_IGNORE_NEW_LINES);
             $randomIndex = rand(0, count($authors) - 1);
 
-            $response->withStatus(200)->write("{\"quote\": \"" . $quotes[$randomIndex] . "\", " .
+            $response->getBody()->write("{\"quote\": \"" . $quotes[$randomIndex] . "\", " .
                 "\"author\": \"" . $authors[$randomIndex] . "\", " .
                 "\"appVersion\": \"1.0.0\", " .
                 "\"environmentName\": \"" . (getenv('ENVIRONMENT_NAME') ?: 'Local') . "\" " .
